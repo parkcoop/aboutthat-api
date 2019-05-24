26e11906afa0ca4f6de56fc231f37a224fac7cec
Parker Cooper <parkr.coopr@gmail.com>
2019-05-24 15:44:43 -0400

    editing post functionality added

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const hbs = require("hbs");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const cors = require("cors");
 
+require("./configs/passport");
 mongoose
   .connect(process.env.MONGO, { useNewUrlParser: true })
   .then(x => {
@@ -38,7 +42,13 @@ app.all("/*", function(req, res, next) {
   next();
 });
 // Express View engine setup
-
+app.use(
+  session({
+    secret: "some secret goes here",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 app.use(
   require("node-sass-middleware")({
     src: path.join(__dirname, "public"),
@@ -46,14 +56,42 @@ app.use(
     sourceMap: true
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+// app.all("/*", function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+//   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+//   if (req.method === "OPTIONS") {
+//     return res.send(204);
+//   }
+//   next();
+// });
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      return callback(null, true);
+    },
+    optionsSuccessStatus: 200,
+    credentials: true
+  })
+);
 // default value for title local
 app.locals.title = "Express - Generated with IronGenerator";
+
+const authRoutes = require("./routes/auth-routes");
+app.use("/api", authRoutes);
 
 const index = require("./routes/index");
 app.use("/", index);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Item = require("../models/item");
+const User = require("../models/user");
+
 /* GET home page */
+
 router.get("/items", (req, res, next) => {
   Item.find().then(items => {
     res.json(items);
@@ -10,20 +13,57 @@ router.get("/items", (req, res, next) => {
 });
 
 router.get("/items/search", (req, res, next) => {
-  console.log(req.query.term);
+  // console.log(req.query.term);
   let searchTerm = req.query.term.toLowerCase();
-  console.log(searchTerm);
+  // console.log(searchTerm);
   let expression = new RegExp(searchTerm, "i");
   Item.find({ name: { $regex: expression } }).then(found => {
-    console.log(found);
+    // console.log(found);
     res.json(found);
   });
 });
 
 router.get("/items/:id", (req, res, next) => {
   Item.findById(req.params.id).then(foundItem => {
-    console.log(foundItem);
+    // console.log(foundItem);
     res.json(foundItem);
+  });
+});
+
+router.post("/items/edit/:id", (req, res, next) => {
+  let newDescriptionArray = [];
+  let newSourceArray = [];
+  console.log(req.body);
+  Item.findById(req.params.id)
+    .then(itemToUpdate => {
+      newDescriptionArray = itemToUpdate.description;
+      newSourceArray = itemToUpdate.sources;
+      newDescriptionArray.push(req.body.description);
+      newSourceArray.push(req.body.source);
+      console.log("new description: " + newDescriptionArray);
+      console.log("new source: " + newSourceArray);
+      Item.findByIdAndUpdate(req.params.id, {
+        description: newDescriptionArray,
+        sources: newSourceArray
+      })
+        .then(somethingElse => {
+          res.json({ final: somethingElse });
+        })
+        .catch(err => console.log(err));
+      // console.log("new description:" newDescriptionArray);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  // Item.findByIdAndUpdate(req.params.id, {
+  //   description: newDescriptionArray
+  // });
+  // console.log("final description", newDescriptionArray);
+});
+
+router.get("/users/:username", (req, res, next) => {
+  User.findOne({ username: req.params.username }).then(user => {
+    res.json(user);
   });
 });
 
