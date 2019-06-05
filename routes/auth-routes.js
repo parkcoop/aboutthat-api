292b78444d7aa0d18d1cab0b6a2999e162ded151
Parker Cooper <parkr.coopr@gmail.com>
2019-06-04 22:00:38 -0400

    code cleanup

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,16 +4,7 @@ const cors = require("cors");
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
 
-// require the user model !!!!
 const User = require("../models/user");
-
-// authRoutes.use(cors());
-// authRoutes.options("*", cors());
-// authRoutes.all("/*", function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   next();
-// });
 
 authRoutes.post("/signup", (req, res, next) => {
   const username = req.body.username;
@@ -59,16 +50,12 @@ authRoutes.post("/signup", (req, res, next) => {
         return;
       }
 
-      // Automatically log in user after sign up
-      // .login() here is actually predefined passport method
       req.login(aNewUser, err => {
         if (err) {
           res.status(500).json({ message: "Login after signup went bad." });
           return;
         }
 
-        // Send the user's information to the frontend
-        // We can use also: res.status(200).json(req.user);
         res.status(200).json(aNewUser);
       });
     });
@@ -85,33 +72,27 @@ authRoutes.post("/login", (req, res, next) => {
     }
 
     if (!theUser) {
-      // "failureDetails" contains the error messages
-      // from our logic in "LocalStrategy" { message: '...' }.
       res.status(401).json(failureDetails);
       return;
     }
 
-    // save user in session
     req.login(theUser, err => {
       if (err) {
         res.status(500).json({ message: "Session save went bad." });
         return;
       }
 
-      // We are now logged in (that's why we can also send req.user)
       res.status(200).json(theUser);
     });
   })(req, res, next);
 });
 
 authRoutes.post("/logout", (req, res, next) => {
-  // req.logout() is defined by passport
   req.logout();
   res.status(200).json({ message: "Log out success!" });
 });
 
 authRoutes.get("/loggedin", (req, res, next) => {
-  // req.isAuthenticated() is defined by passport
   if (req.isAuthenticated()) {
     res.status(200).json(req.user);
     return;
--- routes/index.js
+++ routes/index.js
@@ -9,23 +9,19 @@ router.get("/items", (req, res, next) => {
   Item.find().then(items => {
     res.json(items);
   });
-  // res.render("index");
 });
 
 router.get("/items/search", (req, res, next) => {
-  // console.log(req.query.term);
   let searchTerm = req.query.term.toLowerCase();
-  // console.log(searchTerm);
+
   let expression = new RegExp(searchTerm, "i");
   Item.find({ name: { $regex: expression } }).then(found => {
-    // console.log(found);
     res.json(found);
   });
 });
 
 router.get("/items/:id", (req, res, next) => {
   Item.findById(req.params.id).then(foundItem => {
-    // console.log(foundItem);
     res.json(foundItem);
   });
 });
